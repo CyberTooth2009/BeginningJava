19357e168619adad5df3186b8fcccf22a7b02a8b
jp <26-028@sanignacio.pr>
2023-10-03 19:26:28 -0400

    my first commit

--- src/practice2.java
+++ src/practice2.java
@@ -10,19 +10,41 @@ public class practice2 {
         int rows = 0;
         int cols = 0;
         int num = 0;
-        int txt = 0;
+        String txt;
         Scanner input = new Scanner(System.in);
-        //Ask user for input
+        //Ask user for the number of rows
         System.out.println("Type the number of rows (greater than 2): ");
         rows = input.nextInt();
+        //if the inputted number is less than or equal to 2, display an error and ask the user for a number greater than 2
+        if (rows <= 2){
+           for (int x= rows; x<=2;) {
+               System.out.println("Please type a number greater than 2: ");
+               rows = input.nextInt();
+               if (rows > 2){
+                   break;
+               }
+            }
+        }
+        //Ask user for the number of columns
         System.out.println("Type the number of columns (greater than 2): ");
         cols = input.nextInt();
+        //if the inputted number is less than or equal to 2, display an error and ask the user for a number greater than 2
+        if (cols <= 2){
+            for (int x= cols; x<=2;) {
+                System.out.println("Please type a number greater than 2: ");
+                cols = input.nextInt();
+                if (cols > 2){
+                    break;
+                }
+            }
+        }
+        //Ask user for the character of the rectangle
         System.out.println("Type the character for the rectangle: ");
-        txt = input.nextInt();
-
+        txt = input.next();
+//Draw the triangle based on the conditions given
         for (int i= rows; i>= 1; i--){
             for (int j = 1; j <= cols; j++) {
-                System.out.println("%");
+                System.out.print(txt);
             }
             System.out.println("");
         }
--- src/practice3.java
+++ src/practice3.java
@@ -1,23 +1,28 @@
 //Code by Juan Pablo Jiménez
 //Grade 10
-
 import java.util.Scanner;
-
-public class practice1 {
+public class practice3 {
     public static <string> void main(String[] args) {
-        System.out.println("This program is a number triangle");
+        System.out.println("This program prints a triangle that adds each number by one");
         //Number of rows and columns
         int rows = 0;
+        int i = 1;
         Scanner input = new Scanner(System.in);
         //Ask user for input
         System.out.println("Type the number of rows: ");
         rows = input.nextInt();
 //make the triangle depending on the amount of rows
-        for (int x = 0; x <= rows; x++){
-            for (int y = 1; y <= x; y++){
-                System.out.print(y);
+        /*for (int x = 0; x <= rows; x++) {
+            for (int y = 1; y <= x; y++) {
+                    System.out.print(y);
+            }
+            System.out.println(" ");
+        } */
+        for (int x = 0; x <= rows; x++) {
+            for (int y = 1; y <= x; y++) {
+                System.out.print(i++);
             }
             System.out.println(" ");
         }
-        }
     }
+}
