14d10fd3ab35518fd67f3103377a9fe5066382e1
CyberTooth2009 <106486182+CyberTooth2009@users.noreply.github.com>
2023-10-23 21:32:49 -0400

    more do-while loops

--- src/loopsPractice/doWhileloops/doWhileLoops2.java
+++ src/loopsPractice/doWhileloops/doWhileLoops2.java
@@ -1,18 +1,18 @@
-package loopsPractice.whileLoops;
+package loopsPractice.doWhileloops;
+
 
 //Code written by Samuel Ralph
 //Grade: 9th
 
 import java.util.Scanner;
 
-public class whileloops2 {
+public class doWhileLoops2 {
     public static void main(String[] args) {
         //Define variables and libraries
         Scanner in = new Scanner(System.in);
         int input = 0;
         int counter = 1;
         int sum = 0;
-        int prevnum = 0;
 
         //Prompt user for input
         System.out.println("Please input the number of natural numbers you would like: ");
@@ -21,11 +21,13 @@ public class whileloops2 {
         //Run natural number tree of actions
         System.out.println("You selected the number " + input);
         System.out.println("The first " + input + " natural numbers are:");
-        while(counter != input + 1){
+
+        do {
             System.out.println(counter);
             sum = sum + counter;
             counter++;
         }
+        while(counter != input + 1);
 
         System.out.println("The Sum of Natural Number up to " + input + " terms is " + sum);
 
