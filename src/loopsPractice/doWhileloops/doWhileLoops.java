72323d2061d732cb718db90bdfc4cb4eb96ff486
CyberTooth2009 <106486182+CyberTooth2009@users.noreply.github.com>
2023-10-23 21:28:20 -0400

    started working with do-while loops

--- src/loopsPractice/doWhileloops/doWhileLoops.java
+++ src/loopsPractice/doWhileloops/doWhileLoops.java
@@ -1,4 +1,24 @@
 package loopsPractice.doWhileloops;
 
+//Code written by Samuel Ralph
+//Grade: 9th
+
 public class doWhileLoops {
-}
+    public static void main(String[] args) {
+        //Define variables
+        int counter = 1;
+
+        //Run do-while loop
+        System.out.println("The first 10 natural numbers are: \n");
+
+
+        do {
+            System.out.println(counter);
+            counter++;
+        }
+        while(counter <= 10);
+        }
+
+
+    }
+
