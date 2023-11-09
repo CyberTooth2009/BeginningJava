23663a137522fd82d10644507b2ae29d56597688
CyberTooth2009 <106486182+CyberTooth2009@users.noreply.github.com>
2023-11-09 09:33:15 -0400

    starting with strings

---
+++ src/StringsInJava/Main.java
@@ -0,0 +1,18 @@
+package StringsInJava;
+
+//Code written by Samuel Ralph
+//Grade: 9th
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Declare variables and libraries
+        String str1 = "A fox jumped out one winter's night, and begged the moon to give him light.";
+
+
+
+
+    }
+}
---
+++ src/StringsInJava/StringOne.java
@@ -0,0 +1,12 @@
+package StringsInJava;
+
+import java.util.Scanner;
+
+public class StringOne {
+    public static void main(String[] args) {
+     //Define variables and libraries
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
