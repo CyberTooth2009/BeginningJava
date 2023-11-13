cb34219eb7055666b9739f1077798eab43341ecb
CyberTooth2009 <106486182+CyberTooth2009@users.noreply.github.com>
2023-11-13 09:32:43 -0400

    doing more methods and trees 🌲

--- src/StringsInJava/Main.java
+++ src/StringsInJava/Main.java
@@ -10,9 +10,29 @@ public class Main {
 
         //Declare variables and libraries
         String str1 = "A fox jumped out one winter's night, and begged the moon to give him light.";
+        Scanner in = new Scanner(System.in);
+        String badchar = "";
+
+        System.out.println("The string given is: 'A fox jumped out one winter's night, and begged the moon to give him light.'");
+        System.out.println("Please input the character you would like to remove: ");
+        badchar = in.next();
+
+        System.out.println("Your corrected string reads: " + removeCharacter(badchar));
 
 
 
+
+
+    }
+    public static String removeCharacter(String str) {
+        String correctedString = "";
+        String str1 = "A fox jumped out one winter's night, and begged the moon to give him light.";
+//        Scanner in = new Scanner(System.in);
+
+
+
+        String strcorrect = str1.replace(str, "");
+        return  strcorrect;
 
     }
 }
