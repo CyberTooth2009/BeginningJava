1dad5033e2af6c26639f81fdf8ad1010a86723d3
CyberTooth2009 <106486182+CyberTooth2009@users.noreply.github.com>
2023-12-03 20:20:20 -0400

    getting started with arrays

---
+++ src/ArraysProject/ArraysProject.java
@@ -0,0 +1,26 @@
+package ArraysProject;
+
+import java.util.Scanner;
+
+public class ArraysProject {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("***************************************");
+        System.out.println("*            Problem Solver           *");
+        System.out.println("***************************************");
+        System.out.println("This program offers a large variety of different problem solvers.\n Selected the one you want below by inputting the corresponding number");
+
+
+        System.out.println("------Menu------");
+        System.out.println("1. Problem Solution 1");
+        System.out.println("2. Problem Solution 2");
+        System.out.println("3. Problem Solution 3");
+        System.out.println("0. QUIT");
+
+        System.out.println("Enter your choice: ");
+        int choice = in.nextInt();
+
+    }
+
+    
+}
